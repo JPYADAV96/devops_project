ba7f1cccc8b2b3faf9cf03c2f487f4c0dcd05b7d
vikash <subhashchandrayadav698@gmail.com>
2024-08-12 14:27:47 +0530

    update infra/rds

--- infra/load-balancer/main.tf
+++ infra/load-balancer/main.tf
@@ -15,15 +15,15 @@ variable "dev_proj_1_acm_arn" {}
 variable "lb_target_group_attachment_port" {}
 
 output "aws_lb_dns_name" {
-  value = aws_lb.dev_proj_1_lb.dns_name
+  value = aws_lb.dev_proj_1_lb_unique.dns_name
 }
 
 output "aws_lb_zone_id" {
-  value = aws_lb.dev_proj_1_lb.zone_id
+  value = aws_lb.dev_proj_1_lb_unique.zone_id
 }
 
 
-resource "aws_lb" "dev_proj_1_lb" {
+resource "aws_lb" "dev_proj_1_lb_unique" {
   name               = var.lb_name
   internal           = var.is_external
   load_balancer_type = var.lb_type
@@ -44,7 +44,7 @@ resource "aws_lb_target_group_attachment" "dev_proj_1_lb_target_group_attachment
 }
 
 resource "aws_lb_listener" "dev_proj_1_lb_listner" {
-  load_balancer_arn = aws_lb.dev_proj_1_lb.arn
+  load_balancer_arn = aws_lb.dev_proj_1_lb_unique.arn
   port              = var.lb_listner_port
   protocol          = var.lb_listner_protocol
 
@@ -56,7 +56,7 @@ resource "aws_lb_listener" "dev_proj_1_lb_listner" {
 
 # https listner on port 443
 resource "aws_lb_listener" "dev_proj_1_lb_https_listner" {
-  load_balancer_arn = aws_lb.dev_proj_1_lb.arn
+  load_balancer_arn = aws_lb.dev_proj_1_lb_unique.arn
   port              = var.lb_https_listner_port
   protocol          = var.lb_https_listner_protocol
   ssl_policy        = "ELBSecurityPolicy-FS-1-2-Res-2019-08"
