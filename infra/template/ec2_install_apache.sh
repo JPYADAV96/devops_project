a9263ab125368b5f2fd80df5c251a565eb6dda8f
vikash <subhashchandrayadav698@gmail.com>
2024-08-14 16:23:52 +0530

    ec2 file

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
+
 # shellcheck disable=SC2164
 cd /home/ubuntu
 yes | sudo apt update
 yes | sudo apt install python3 python3-pip
 git clone https://github.com/JPYADAV96/python-mysql-db-proj-1.git
 sleep 20
+
 # shellcheck disable=SC2164
 cd python-mysql-db-proj-1
-
 
 # Install necessary packages for installing Docker over HTTPS
 sudo apt-get install -y \
@@ -30,16 +31,26 @@ sudo apt-get update
 # Install Docker Engine, CLI, and containerd
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-
 # Optional: Add the current user to the docker group to run Docker without sudo
 sudo usermod -aG docker $USER
 
 # Print a success message
 echo "Docker has been installed successfully. Please log out and log back in to apply group changes if you added yourself to the docker group."
 
-#sudo chmod 666 /var/run/docker.sock
+# Give Docker permission to access the Docker socket
+sudo chmod 666 /var/run/docker.sock
 
+# Ensure Docker is running
+sudo systemctl start docker
+
+# Verify Docker installation
+if ! docker --version; then
+    echo "Docker installation failed!"
+    exit 1
+fi
+
+# Build Docker image
 docker build -t flask-app .
 
-
+# Run Docker container
 docker run -d -p 5000:5000 flask-app:latest
