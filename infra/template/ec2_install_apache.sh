d9ddc6f1adcaff863bb385c37f78b28de859cd7b
vikash <subhashchandrayadav698@gmail.com>
2024-08-14 17:29:43 +0530

    ec2 file

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -8,14 +8,11 @@ yes | sudo apt update
 yes | sudo apt install -y python3 python3-pip
 
 # Clone the repository
-if ! git clone https://github.com/JPYADAV96/python-mysql-db-proj-1.git; then
-    echo "Failed to clone repository!"
-    exit 1
-fi
+git clone https://github.com/JPYADAV96/python-mysql-db-proj-1.git
 
-sleep 20
 
-cd python-mysql-db-proj-1
+
+
 
 # Install necessary packages for Docker
 sudo apt-get install -y \
@@ -25,10 +22,9 @@ sudo apt-get install -y \
     lsb-release
 
 # Add Docker's official GPG key
-if ! curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg; then
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "Failed to add Docker's GPG key!"
-    exit 1
-fi
+
 
 # Set up the stable Docker repository
 echo \
@@ -39,15 +35,12 @@ echo \
 sudo apt-get update
 
 # Install Docker Engine, CLI, and containerd
-if ! sudo apt-get install -y docker-ce docker-ce-cli containerd.io; then
-    echo "Failed to install Docker!"
-    exit 1
-fi
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Add the current user to the docker group (ensure user re-login is required)
 sudo chmod 666 /var/run/docker.sock
 
-
+cd python-mysql-db-proj-1
 # Build Docker image
 sudo docker build -t flask-app .
 # Run Docker container
