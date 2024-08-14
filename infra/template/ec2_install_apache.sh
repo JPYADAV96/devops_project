32606d4efc2d0be7d5c7b91d606c03c70e9e47d9
vikash <subhashchandrayadav698@gmail.com>
2024-08-14 15:51:52 +0530

    ec2 file

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -7,9 +7,37 @@ git clone https://github.com/JPYADAV96/python-mysql-db-proj-1.git
 sleep 20
 # shellcheck disable=SC2164
 cd python-mysql-db-proj-1
-pip3 install -r requirements.txt
-echo 'Waiting for 30 seconds before running the app.py'
-setsid python3 -u app.py &
+
+
+# Install necessary packages for installing Docker over HTTPS
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Set up the stable Docker repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update the package list to include Docker packages
+sudo apt-get update
+
+# Install Docker Engine, CLI, and containerd
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+
+# Optional: Add the current user to the docker group to run Docker without sudo
+sudo usermod -aG docker $USER
+
+# Print a success message
+echo "Docker has been installed successfully. Please log out and log back in to apply group changes if you added yourself to the docker group."
+
+#sudo chmod 666 /var/run/docker.sock
 
 docker build -t flask-app .
 
